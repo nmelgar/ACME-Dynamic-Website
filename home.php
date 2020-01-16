fd248f298c792072530e4d49658c9fb5fcd2ad35
nmelgar <nmelgar16013@gmail.com>
2020-01-16 07:08:57 -0600

    16-Ene--7:00am

--- home.php
+++ home.php
@@ -1,4 +1,28 @@
-<?php
-    echo("hello world");
-    echo("hola");
-?>
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    </head>
+
+    <body>
+        <div id="container">
+            <header>
+                <nav>
+                                
+                </nav>
+            </header>
+            <main>
+                <h1>Welcome to Acme!</h1>
+                <section id="rocketfeature">
+                    <img src="images/site/rocketfeature.jpg" alt="">
+                </section>
+            </main>
+            <section class="reviews">
+
+            </section>
+            <section class="recipes">
+
+            </section>
+        </div>
+    </body>
