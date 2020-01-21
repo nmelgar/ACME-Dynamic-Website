1cff03b72f906e3e7eb7bf943b1c785b11ed2238
nmelgar <nmelgar16013@gmail.com>
2020-01-21 09:28:32 -0600

    21-Ene--9:28AM

--- home.php
+++ home.php
@@ -13,9 +13,9 @@
             </header>
             <main>
                 <h1>Welcome to Acme!</h1>
-                <section class="coyoterocket">
-                        <img class="coyoteimage" src="images/site/rocketfeature.jpg" alt="A coyote riding a rocket">
-<!--                    <div class="coyoterocket-content">-->
+                <article class="coyoterocket">
+                    <img class="coyoteimage" src="images/site/rocketfeature.jpg" alt="A coyote riding a rocket">
+                    <div class="coyotetext-block">
                         <ul>
                             <li><h2>Acme Rocket</h2></li>
                             <li>Quick lighting fuse</li>
@@ -23,13 +23,40 @@
                             <li>Mobile launch stand included</li>
                             <li><a class="iwantit" href="/acme/cart/"><img class="iwantit" id="actionbtn" alt="Add to cart button" src="images/site/iwantit.gif"></a></li>
                         </ul>
-<!--                    </div>-->
-                </section>
-                
-              <div class="sections">
-                
-                <!--REVIEWS SECTION----------------->
-                <section class="reviews">
+                    </div>
+                </article>             
+            </main>
+            <section class="row">
+                <div class="column" id="left-section">
+                    <h3>Featured Recipes</h3>
+                    <table>
+                        <tr>
+                            <th>
+                            <img src="images/recipes/bbqsand.jpg" alt="Roadrunner BBQ">
+                            <br />
+                            Pulled Roadrunner BBQ
+                            </th>
+                            <th>
+                            <img src="images/recipes/potpie.jpg" alt="Roadrunner Pot Pie">
+                            <br />
+                            Roadrunner Pot Piet
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>
+                            <img src="images/recipes/soup.jpg" alt="Roadrunner soup">
+                            <br />
+                            Roadrunner Soup
+                            </th>
+                            <th>
+                            <img src="images/recipes/taco.jpg" alt="Roadrunner taco">
+                            <br />
+                            Roadrunner Tacos
+                            </th>
+                        </tr>
+                    </table>
+                </div>
+                <div class="column" id="right-right">
                     <h3>Acme Rocket Reviews</h3>
                     <ul>
                         <li>"I don't know how I ever caught roadrunners before this." (4/5)</li>
@@ -38,44 +65,8 @@
                         <li>"I didn't even have to pull the meat apart." (4.5/5)</li>
                         <li>"I'm on my thirtieth one. I love these things!" (5/5)</li>
                     </ul>
-
-                </section>
-                  
-                <!--RECIPES SECTION----------------->
-            <section class="recipes">
-                <h3>Featured Recipes</h3>
-                <table>
-                    <tr>
-                        <th>
-                        <img src="images/recipes/bbqsand.jpg" alt="Roadrunner BBQ">
-                        <br />
-                        Pulled Roadrunner BBQ
-                        </th>
-                        <th>
-                        <img src="images/recipes/potpie.jpg" alt="Roadrunner Pot Pie">
-                        <br />
-                        Roadrunner Pot Piet
-                        </th>
-                    </tr>
-                    <tr>
-                        <th>
-                        <img src="images/recipes/soup.jpg" alt="Roadrunner soup">
-                        <br />
-                        Roadrunner Soup
-                        </th>
-                        <th>
-                        <img src="images/recipes/taco.jpg" alt="Roadrunner taco">
-                        <br />
-                        Roadrunner Tacos
-                        </th>
-                    </tr>
-                    
-                </table>
-
-            </section>
-
                 </div>
-            </main>
+            </section>
 
  
             <footer> <?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/footer.php'; ?>
