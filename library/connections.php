552f4c3fe55655fe717178a172ed64ab848f8332
nmelgar <nmelgar16013@gmail.com>
2020-01-23 12:16:06 -0600

    23-Ene--12:15pm PDO trys

---
+++ library/connections.php
@@ -0,0 +1,21 @@
+<?php
+
+function acmeConnect(){
+    $server = 'localhost';
+    $dbname= 'acme';
+    $username = 'iClient';
+    $password = 'HYLP7EXAy4jtA5Sr';
+    $dsn = "mysql:host=$server;dbname=$dbname";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+   
+    // Create the actual connection object and assign it to a variable
+    try {
+     $link = new PDO($dsn, $username, $password, $options);
+     return $link;
+    } catch(PDOException $e) {
+     header('Location: /acme/view/500.php');
+     exit;
+    }
+   }
+
+acmeConnect();
