004d4aee62fe12fccd7f7145dcd7eb8869e1ecc9
nmelgar <nmelgar16013@gmail.com>
2020-01-25 20:59:54 -0600

    25-Ene--8:59pm Header's images root changed

--- common/header.php
+++ common/header.php
@@ -1,9 +1,9 @@
 <div class="header">
     <div class="header-left">
-        <a href="#" class="logo"><img id="logo" src="images/site/logo.gif" alt="Acme logo"></a>
+        <a href="#" class="logo"><img id="logo" src="/acme/images/site/logo.gif" alt="Acme logo"></a>
     </div>    
     <div class="header-right">
-        <a href="#" class="account"><img id="account" src="images/site/account.gif" alt="Image of a folder"> My Account</a>
+        <a href="#" class="account"><img id="account" src="/acme/images/site/account.gif" alt="Image of a folder"> My Account</a>
     </div>            
 </div>
 <nav>
