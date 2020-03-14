7c725f4775dead60c224b6f66223913aa2b5d9c2
nmelgar <nmelgar16013@gmail.com>
2020-03-14 10:55:31 -0600

    14-Mar--10:55am

--- view/category.php
+++ view/category.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title><?php echo $categoryName; ?> Products | Acme, Inc.</title>
+    <title> Products | Acme, Inc.</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" type="text/css" href="/acme/css/style.css">
 </head>
